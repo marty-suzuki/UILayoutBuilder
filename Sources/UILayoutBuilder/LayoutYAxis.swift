ca712c8e8ea50ca5c733a3e372c3236a8ce24cda
marty-suzuki <s1180183@gmail.com>
2020-02-05 02:06:09 +0900

    Add LayoutYAxis

---
+++ Sources/UILayoutBuilder/LayoutYAxis.swift
@@ -0,0 +1,50 @@
+//
+//  LayoutYAxis.swift
+//  UILayoutBuilder
+//
+//  Created by marty-suzuki on 2020/02/05.
+//
+
+import UIKit
+
+public struct LayoutYAxis {
+    public typealias RawAnchor = NSLayoutYAxisAnchor
+
+    public var equalTo: Relation {
+        .init(toAnchor: { [rawAnchor] in rawAnchor.constraint(equalTo: $0, constant: $1) })
+    }
+
+    public var greaterThanOrEqualTo: Relation {
+        .init(toAnchor: { [rawAnchor] in rawAnchor.constraint(greaterThanOrEqualTo: $0, constant: $1) })
+    }
+
+    public var lessThanOrEqualTo: Relation {
+        .init(toAnchor: { [rawAnchor] in rawAnchor.constraint(lessThanOrEqualTo: $0, constant: $1) })
+    }
+
+    private let rawAnchor: RawAnchor
+
+    init(_ view: UIView, for keyPath: KeyPath<UIView, RawAnchor>) {
+        self.rawAnchor = view[keyPath: keyPath]
+    }
+
+    public struct Relation {
+        fileprivate let toAnchor: (RawAnchor, CGFloat) -> NSLayoutConstraint
+
+        public func anchor(_ anchor: LayoutYAxis) -> ToAnchor {
+            .init(constraint: { [toAnchor] in toAnchor(anchor.rawAnchor, $0) })
+        }
+    }
+
+    public struct ToAnchor: ConstraintBuilder {
+        fileprivate let constraint: (CGFloat) -> NSLayoutConstraint
+
+        public func build() -> NSLayoutConstraint {
+            constraint(0)
+        }
+
+        public func constant(_ constant: CGFloat) -> ConstraintBuilder {
+            AnyConstraintBuilder(constraint: { [constraint] in constraint(constant) })
+        }
+    }
+}
