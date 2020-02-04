5eea0a16edd7270331371f9f7c13eb404a6a07db
marty-suzuki <s1180183@gmail.com>
2020-02-05 02:14:17 +0900

    Add Example

---
+++ Example/UILayoutBuilderApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UILayoutBuilderApp
+//
+//  Created by marty-suzuki on 2020/02/05.
+//  Copyright © 2020 marty-suzuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
