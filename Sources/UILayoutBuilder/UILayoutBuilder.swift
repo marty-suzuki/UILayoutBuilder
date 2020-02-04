a69fb1891a58e14dde8951c54ced2926484ad683
marty-suzuki <s1180183@gmail.com>
2020-02-05 02:19:43 +0900

    Add UILayoutBuilderExtension

--- Sources/UILayoutBuilder/UILayoutBuilder.swift
+++ Sources/UILayoutBuilder/UILayoutBuilder.swift
@@ -1,3 +1,34 @@
-struct UILayoutBuilder {
-    var text = "Hello, World!"
+//
+//  UILayoutBuilder.swift
+//  UILayoutBuilder
+//
+//  Created by marty-suzuki on 2020/02/05.
+//
+
+import UIKit
+
+public protocol UILayoutBuilderCompatible {
+    associatedtype UILayoutBuilderBase
+    var ulb: UILayoutBuilderExtension<UILayoutBuilderBase> { get }
+}
+
+extension UILayoutBuilderCompatible {
+    public var ulb: UILayoutBuilderExtension<Self> { .init(self) }
+}
+
+public struct UILayoutBuilderExtension<Base> {
+    let base: Base
+
+    fileprivate init(_ base: Base) {
+        self.base = base
+    }
+}
+
+extension UIView: UILayoutBuilderCompatible {}
+
+extension UILayoutBuilderExtension where Base: UIView {
+
+    public func add(handler: (ViewProxy<UIView>) -> Void) {
+        handler(ViewProxy(base))
+    }
 }
